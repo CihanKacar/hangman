770c7b642c03610f4d621905cb153244e82ce01c
Cihan Aziz Kacar <cihankacar92@gmail.com>
2021-12-03 13:42:12 +0100

    Implement base Game

--- src/main.py
+++ src/main.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python3
 
+
+import os
 import random
+import platform
+
+def clear():
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
 
 def main():
-    words  = ["yarak", "amcik", "oruspu", "cihan"]
-    random_word = random.choice(words)
+    words  = ["Cihan", "Ibo", "Murat", "Ali"]
+    word = random.choice(words)
+    guess_me =  list(word)
+    guessed = ['?' for _ in range(len(guess_me))]
+    MAXLP = 10
+    lp = MAXLP
 
-    chars = list(random_word)
+
+
+    while ('?' in guessed and lp>0):
+        print(f"{' '.join(guessed).capitalize()} {lp}")
+        guess = input(">>> ")[0].upper()
+        correct = False
+
+        for i in range(len(guess_me)):
+            if guess == guess_me[i].upper():
+                guessed[i] = guess
+                correct = True
+        if correct == False:
+            lp = lp - 1
+
+
+        clear()
+
+    print("You {}! The word was {}".format('won' if lp > 0 else 'lost', word))
 
 
 
